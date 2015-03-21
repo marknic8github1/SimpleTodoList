1d274e4aa19e819d85f73cb90d95387e13d9a0ef
marknic8github1 <marknic8github1@pacbell.net>
2015-03-21 11:15:16 -0700

    Initial Commit

---
+++ app/src/main/java/com/gabiq/simpletodolist1/TodoActivity.java
@@ -0,0 +1,113 @@
+package com.gabiq.simpletodolist1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class TodoActivity extends ActionBarActivity {
+    ListView lvItems;
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        //items = new ArrayList<String>(); // comment since loading now
+        //Load items onCreate from file
+        readItems(); // line added to read from file
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        //items.add("First Item"); // comment since loading now
+        //items.add("Second Item"); // comment since loading now
+        setupListViewListener();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_todo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+// Below method might be commented out since adding Save items
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        saveItems(); // write to file
+    }
+// Below method might be commented out since adding Save items
+/*    public void addTodoItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        saveItems(); // write to file
+    }
+*/
+    public void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent,
+                                           View view, int position, long rowId) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                saveItems(); // write to file
+                return true;
+            }
+        });
+    }
+
+    //These two methods will add the ability to load/save from files!
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+            e.printStackTrace();
+        }
+    }
+    private void saveItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

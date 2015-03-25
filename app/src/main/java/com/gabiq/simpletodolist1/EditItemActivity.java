5e554040b3211b7122349ef65dc93fd7fa1e9596
marknic8github1 <marknic8github1@pacbell.net>
2015-03-24 17:53:39 -0700

    Draft 1 meets program requirements about creating a second activity and being able to edit an item. Then store that item in the proper location in the original activity list.

---
+++ app/src/main/java/com/gabiq/simpletodolist1/EditItemActivity.java
@@ -0,0 +1,73 @@
+package com.gabiq.simpletodolist1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class EditItemActivity extends ActionBarActivity {
+
+    long rowId = 0; // placed here so visible in onCreate and onEditItem
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        // ActivityTwo.java (subactivity) can access any extras passed in
+        String position = getIntent().getStringExtra("position");
+        //String rowId = getIntent().getStringExtra("rowId");
+        rowId = getIntent().getLongExtra("rowId", 0);
+        Toast.makeText(this, "rowID = " + rowId, Toast.LENGTH_SHORT).show(); // for testing
+        EditText etEditItem = (EditText) findViewById(R.id.editView);
+        etEditItem.setText(position); // sets edit box to selected text
+        // Now put curser at end of text
+        int positionOfCurser = position.length();
+        etEditItem.setSelection(positionOfCurser); // puts curser at end of text
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Send data back to first activity from second activity
+    public void onEditItem(View v) { // hitting the Save button here
+    //    EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+    //    String itemText = etNewItem.getText().toString();
+    //    itemsAdapter.add(itemText);
+    //    etNewItem.setText(""); // clears line for another entry
+    //    saveItems(); // write to file
+        EditText etEditItem = (EditText) findViewById(R.id.editView);
+        Intent data = new Intent();
+        // Pass relevant data back as a result
+        data.putExtra("position", etEditItem.getText().toString());
+        //data.putExtra("rowId", etEditItem.getText().toString());
+        data.putExtra("rowId", rowId); // ints work too
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, data); // set result code and bundle data for response
+        finish(); // closes the activity, pass data to parent
+    }
+
+}
--- app/src/main/java/com/gabiq/simpletodolist1/TodoActivity.java
+++ app/src/main/java/com/gabiq/simpletodolist1/TodoActivity.java
@@ -1,7 +1,8 @@
 package com.gabiq.simpletodolist1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -35,9 +37,10 @@ public class TodoActivity extends ActionBarActivity {
         lvItems.setAdapter(itemsAdapter);
         //items.add("First Item"); // comment since loading now
         //items.add("Second Item"); // comment since loading now
-        setupListViewListener();
+        setupListViewListener(); // this listener is for a long click
+        setupListViewEditListener(); // added to run listener for click, works!
+        // above line results in second screen opening up successfully
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,26 +60,18 @@ public class TodoActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
+    return super.onOptionsItemSelected(item);
     }
-// Below method might be commented out since adding Save items
+
+// Below method might be edited since adding Save items
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
-        etNewItem.setText("");
+        etNewItem.setText(""); // clears line for another entry
         saveItems(); // write to file
     }
-// Below method might be commented out since adding Save items
-/*    public void addTodoItem(View v) {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
-        etNewItem.setText("");
-        saveItems(); // write to file
-    }
-*/
+
     public void setupListViewListener() {
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -108,6 +103,57 @@ public class TodoActivity extends ActionBarActivity {
             FileUtils.writeLines(todoFile, items);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+// Add Edit Item Capability
+    public void setupListViewEditListener() {
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int position, long rowId) {
+                // first parameter is the context, second is the class of the activity to launch
+                Intent i = new Intent(TodoActivity.this, EditItemActivity.class);
+                // put "extras" into the bundle for access in the second activity
+                String positionText = items.get(position);
+                i.putExtra("position", positionText);// pass data to launched activity
+                i.putExtra("rowId", rowId);// pass data to launched activity
+                // REQUEST_CODE can be any value we like, used to determine the result type later
+                int REQUEST_CODE = 0; // variable to get a result back from the second activity
+                //startActivity(i); // brings up the second activity
+                startActivityForResult(i, REQUEST_CODE); // brings up the second activity to get a result
+
+                return;
+            }
+        });
+    }
+
+    // ActivityOne.java, time to handle the result of the sub-activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK ) { // && requestCode == REQUEST_CODE
+            // Extract name value from result extras
+            String position = data.getExtras().getString("position");
+            //String rowId = data.getExtras().getString("rowId");
+            long rowId = data.getExtras().getLong("rowId");
+            //int code = data.getExtras().getInt("code", 0);
+            // Toast the name to display temporarily on screen
+            //Toast.makeText(this, position, Toast.LENGTH_SHORT).show(); // testing, works!
+            Toast.makeText(this, "rowId = " + rowId, Toast.LENGTH_SHORT).show(); // testing, works!
+            //EditText etEditedItem = (EditText) findViewById(R.id.etNewItem);
+            // android.R.layout.simple_list_item_1
+
+            //ListView etEditedItem = (ListView) findViewById(R.id.lvItems);
+
+            //EditText etEditedItem = (EditText) findViewById(R.id.rowId);
+
+            //etEditedItem.setText(position); // attempt to set text on proper line, does not work
+
+            items.set((int)rowId,position); // This edits the proper line! Had to put (int) in.
+
+            //String position1result = items.put(position);
+            saveItems(); // write to file
         }
     }
 }
